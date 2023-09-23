7dbfbf229299e1f9bca70f8e93cabf58990bc575
EnesHetemi <eneshetemi3@gmail.com>
2023-09-23 14:32:56 +0200

    Shtimi i Views

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,10 +8,14 @@ namespace eTickets.Models
 
         public int Id { get; set; }
 
+        [Display(Name = "Profile Picture")]
+
         public string? ProfilePictureURL { get; set; }
 
+        [Display(Name = "Full Name")]
         public string? FullName { get; set; }
 
+        [Display(Name = "Biography")]
         public string? Bio { get; set; }
 
         //Relationships
