02a7589d8e86c67cd300e7f34060dde566261821
EnesHetemi <eneshetemi3@gmail.com>
2023-09-26 14:31:14 +0200

    ActorServices dhe GetAll Actors

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,9 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Conf
     ("DefaultConnectionString")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Services Configuration
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 
